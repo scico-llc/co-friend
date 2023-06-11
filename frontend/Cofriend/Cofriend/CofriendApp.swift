90d0dcf81a57bb1eb68784edc8e616dfffa00606
atsushi.otsubo <m@rirex.net>
2023-06-11 17:01:09 +0900

    Add: Create ios project file

---
+++ frontend/Cofriend/Cofriend/CofriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  CofriendApp.swift
+//  Cofriend
+//
+//  Created by Atsushi Otsubo on 2023/06/11.
+//
+
+import SwiftUI
+
+@main
+struct CofriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

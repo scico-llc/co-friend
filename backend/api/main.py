0e673dc6c4acee8f724118d0779cf0bcb9827bbe
Yodai Hirano <38518123+yodai-hi@users.noreply.github.com>
2023-06-17 11:30:11 +0900

    Revert "Feature/api/add chat api"

--- backend/api/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from routers import characters, chats
-from firebase.firestore import initialize
-
-initialize()
-app = FastAPI()
-app.include_router(characters.router, tags=["characters"])
-app.include_router(chats.router, tags=["chats"])

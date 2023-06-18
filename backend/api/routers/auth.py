564209c15d7adb9cfa29b6d467a3d38dd651e569
yodai-hi <yodai.hirano@sci-co.co.jp>
2023-06-18 09:30:36 +0000

    add: セキュリティを追加

---
+++ backend/api/routers/auth.py
@@ -0,0 +1,14 @@
+import os
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from dotenv import load_dotenv
+
+load_dotenv("../.env", verbose=True)
+api_keys = [os.environ["SERVER_KEY"]]
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")  # use token authentication
+
+def api_key_auth(api_key: str = Depends(oauth2_scheme)):
+    if api_key not in api_keys:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Forbidden"
+        )
--- backend/api/routers/chats.py
+++ backend/api/routers/chats.py
@@ -1,14 +1,14 @@
 import sys
 
 sys.path.append("../")
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 import schemas.chat as sch
 import generate_chat.user_chat as chat
+from . import auth
 
 router = APIRouter()
 
-
-@router.post("/chat")
+@router.post("/chat", dependencies=[Depends(auth.api_key_auth)])
 async def send_chat(reqBody: sch.ChatRequest) -> sch.ChatResponse:
     message = chat.character_chat(
         reqBody.animal_id,
@@ -18,7 +18,7 @@ async def send_chat(reqBody: sch.ChatRequest) -> sch.ChatResponse:
     return sch.ChatResponse(reqBody.animal_id, message)
 
 
-@router.post("/chat/setting")
+@router.post("/chat/setting", dependencies=[Depends(auth.api_key_auth)])
 async def send_chat(reqBody: sch.ChatSetting):
     chat.get_character_setting(
         reqBody.animal_id,

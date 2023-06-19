58eed7ba32d33d30248b7a6b42170709aef9ae98
atsushi.otsubo <m@rirex.net>
2023-06-19 21:57:10 +0900

    add: LocalStorage保存用のUserDefaultsClientを追加

---
+++ frontend/Cofriend/Cofriend/Feature/LocalStorage/UserDefaultsClient.swift
@@ -0,0 +1,43 @@
+//
+//  UserDefaultsClient.swift
+//  Cofriend
+//
+//  Created by Atsushi Otsubo on 2023/06/19.
+//
+
+import Foundation
+
+enum UserDefaultsClient {
+    enum StringItem: String {
+        case animalId
+        case animalImageUrl
+    }
+    
+    private static func set(value: String, forKey item: StringItem) {
+        UserDefaults.standard.set(value, forKey: item.rawValue)
+    }
+    
+    private static func string(forKey item: StringItem) -> String {
+        UserDefaults.standard.string(forKey: item.rawValue) ?? ""
+    }
+}
+
+extension UserDefaultsClient {
+    static var animalId: String {
+        get {
+            return string(forKey: .animalId)
+        }
+        set {
+            set(value: newValue, forKey: .animalId)
+        }
+    }
+    
+    static var animalImageUrl: String {
+        get {
+            return string(forKey: .animalImageUrl)
+        }
+        set {
+            set(value: newValue, forKey: .animalImageUrl)
+        }
+    }
+}
